5ceaf47db61ed56774f36d56f030e87a6beec197
Vladislav Gladkiy <kbga5lh@gmail.com>
2020-03-08 13:59:12 +0300

    Finish camera control system

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,14 @@ use game_engine::color::Color;
 fn main() {
     let event_loop = glutin::event_loop::EventLoop::new();
     let s = event_loop.available_monitors().next().unwrap();
-    let wb = glutin::window::WindowBuilder::new().with_fullscreen(Some(glutin::window::Fullscreen::Borderless(s)));
+    let wb = glutin::window::WindowBuilder::new()
+        .with_fullscreen(Some(glutin::window::Fullscreen::Borderless(s)));
     let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+    {
+        let w = display.gl_window();
+        w.window().set_cursor_visible(false);
+    }
 
     let vertex_shader_src = fs::read_to_string("assets/shaders/vertex_shader.glsl").unwrap();
     let fragment_shader_src = fs::read_to_string("assets/shaders/fragment_shader.glsl").unwrap();
@@ -75,13 +80,16 @@ fn main() {
     let mut angle: f32 = 0.0;
     let speed: f32 = 0.5;
 
-    let mut movement_buttons = [false; 4];
-    let mut view_pos = Vector3::new(0.0, 1.5, -2.0);
+    let mut movement_buttons = [false; 6];
+    let mut view_pos = Vector3::new(0.0, 0.0, -2.0);
+    let mut view_up = Vector3::new(0.0, 1.0, 0.0);
 
     let mut yaw = -90.0;
     let mut pitch = 0.0;
 
     let mut elapsed_time: f32 = 0.0;
+
+    let mut mouse_position = (0.0f32, 0.0f32);
 
     // =======================
     // ====== loop ===========
@@ -103,9 +111,14 @@ fn main() {
                         event::KeyboardInput { virtual_keycode: Some(event::VirtualKeyCode::S), .. } => movement_buttons[1] = e.state == event::ElementState::Pressed,
                         event::KeyboardInput { virtual_keycode: Some(event::VirtualKeyCode::A), .. } => movement_buttons[2] = e.state == event::ElementState::Pressed,
                         event::KeyboardInput { virtual_keycode: Some(event::VirtualKeyCode::D), .. } => movement_buttons[3] = e.state == event::ElementState::Pressed,
+                        event::KeyboardInput { virtual_keycode: Some(event::VirtualKeyCode::LShift), .. } => movement_buttons[4] = e.state == event::ElementState::Pressed,
+                        event::KeyboardInput { virtual_keycode: Some(event::VirtualKeyCode::Space), .. } => movement_buttons[5] = e.state == event::ElementState::Pressed,
                         _ => (),
                     }
                 },
+                event::WindowEvent::CursorMoved { position: e, .. } => {
+                    mouse_position = (e.x as f32, e.y as f32);
+                }
                 _ => return,
             },
             event::Event::NewEvents(cause) => match cause {
@@ -118,6 +131,9 @@ fn main() {
 
         // update
 
+        let frame_size = display.get_framebuffer_dimensions();
+        let mut center = ((frame_size.0 / 2) as f32, (frame_size.1 / 2) as f32);
+
         let params = glium::DrawParameters {
             depth: glium::Depth {
                 test: glium::draw_parameters::DepthTest::IfLess,
@@ -127,10 +143,13 @@ fn main() {
             .. Default::default()
         };
 
-        if movement_buttons[0] { pitch += elapsed_time * 10.0 };
-        if movement_buttons[1] { pitch -= elapsed_time * 10.0 };
-        if movement_buttons[2] { yaw -= elapsed_time * 10.0 };
-        if movement_buttons[3] { yaw += elapsed_time * 10.0 };
+        let sensitivity: f32 = 0.025;
+        let mouse_offset = (
+            (mouse_position.0 - center.0) * sensitivity,
+            (center.1 - mouse_position.1) * sensitivity);
+
+        yaw += mouse_offset.0;
+        pitch += mouse_offset.1;
 
         if pitch > 89.0 {
             pitch =  89.0;
@@ -148,12 +167,21 @@ fn main() {
             -pitch.cos() * yaw.sin(),
         ).normalized();
 
+
+        let delta = elapsed_time * 2.0;
+        let view_right = direction.cross(view_up).normalized();
+        if movement_buttons[0] { view_pos += direction * delta };
+        if movement_buttons[1] { view_pos -= direction * delta };
+        if movement_buttons[2] { view_pos += view_right * delta };
+        if movement_buttons[3] { view_pos -= view_right * delta };
+        if movement_buttons[4] { view_pos.y -= delta };
+        if movement_buttons[5] { view_pos.y += delta };
+
         let view = math::view_matrix(
             view_pos,
             direction,
-            Vector3::new(0.0, 1.0, 0.0));
+            view_up);
 
-        let frame_size = display.get_framebuffer_dimensions();
         let mut renderer = Renderer::new(&display,
             [0.0, 0.0, 0.0],
             [1.4, 0.4, -0.7f32],
@@ -165,6 +193,10 @@ fn main() {
         rook.mesh.transform.set_position(Vector3::new(angle.sin(), 0.0, angle.cos())
             * rook.mesh.transform.get_scale().z * 3.0);
         angle += speed * elapsed_time;
+
+        let w = display.gl_window();
+        let window = w.window();
+        window.set_cursor_position(glutin::dpi::PhysicalPosition { x: center.0, y: center.1 });
 
         // draw
 
